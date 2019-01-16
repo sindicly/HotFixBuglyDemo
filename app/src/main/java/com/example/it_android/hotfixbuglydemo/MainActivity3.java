fe5a51a3bb9fe50507a38ee6e7e9bd92fa9c00bb
sindicly <sindi.chen@coassets.com.cn>
2019-01-16 15:44:34 +0800

    1.16

--- app/src/main/java/com/example/it_android/hotfixbuglydemo/MainActivity3.java
+++ app/src/main/java/com/example/it_android/hotfixbuglydemo/MainActivity3.java
@@ -1,16 +1,16 @@
 package com.example.it_android.hotfixbuglydemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.tencent.bugly.Bugly;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main3);
         Bugly.init(getApplicationContext(), "e7a748ede4", false);
     }
 }
